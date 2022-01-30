a4fd30fbf91cffd8fac013149f62d8bf209fc25c
Łukasz Sutkowski <759478+standardcode@users.noreply.github.com>
2022-01-30 09:29:25 +0100

    Docker compose

--- src/main/java/com/standardcode/springdemo/SpringdemoApplication.java
+++ src/main/java/com/standardcode/springdemo/SpringdemoApplication.java
@@ -2,12 +2,21 @@ package com.standardcode.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringdemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+set -e
+
 ./gradlew jibDockerBuild
+docker compose up --abort-on-container-exit
